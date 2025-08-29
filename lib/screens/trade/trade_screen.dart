7fd5e0a347d9bf00d6359e9f9726062abc0386f6
mazdon9 <chemanhkiett@gmail.com>
2025-08-29 17:31:43 +0700

    commpleted  project Crypto exchanges

--- lib/screens/trade/trade_screen.dart
+++ lib/screens/trade/trade_screen.dart
@@ -260,49 +260,50 @@ class _TradeScreenState extends State<TradeScreen> {
           ),
           const SizedBox(width: 12),
           // Right Column - Trading Controls
-          // Expanded(
-          //   child: TradingControlsWidget(
-          //     isBuySelected: _isBuySelected,
-          //     selectedLimit: _selectedLimit,
-          //     sliderValue: _sliderValue,
-          //     usdtAmount: _usdtAmount,
-          //     btcAmount: _btcAmount,
-          //     onBuySellChanged: (isBuy) {
-          //       setState(() {
-          //         _isBuySelected = isBuy;
-          //       });
-          //     },
-          //     onLimitTap: _showLimitBottomSheet,
-          //     onSliderChanged: (value) {
-          //       setState(() {
-          //         _sliderValue = value;
-          //       });
-          //     },
-          //     onUsdtIncrease: () {
-          //       setState(() {
-          //         _usdtAmount++;
-          //       });
-          //     },
-          //     onUsdtDecrease: () {
-          //       setState(() {
-          //         if (_usdtAmount > 0) _usdtAmount--;
-          //       });
-          //     },
-          //     onBtcIncrease: () {
-          //       setState(() {
-          //         _btcAmount++;
-          //       });
-          //     },
-          //     onBtcDecrease: () {
-          //       setState(() {
-          //         if (_btcAmount > 0) _btcAmount--;
-          //       });
-          //     },
-          //     onBuyTap: () {
-          //       // Implement buy functionality
-          //     },
-          //   ),
-          // ),
+
+          Expanded(
+            child: TradingControlsWidget(
+              isBuySelected: _isBuySelected,
+              selectedLimit: _selectedLimit,
+              sliderValue: _sliderValue,
+              usdtAmount: _usdtAmount,
+              btcAmount: _btcAmount,
+              onBuySellChanged: (isBuy) {
+                setState(() {
+                  _isBuySelected = isBuy;
+                });
+              },
+              onLimitTap: _showLimitBottomSheet,
+              onSliderChanged: (value) {
+                setState(() {
+                  _sliderValue = value;
+                });
+              },
+              onUsdtIncrease: () {
+                setState(() {
+                  _usdtAmount++;
+                });
+              },
+              onUsdtDecrease: () {
+                setState(() {
+                  if (_usdtAmount > 0) _usdtAmount--;
+                });
+              },
+              onBtcIncrease: () {
+                setState(() {
+                  _btcAmount++;
+                });
+              },
+              onBtcDecrease: () {
+                setState(() {
+                  if (_btcAmount > 0) _btcAmount--;
+                });
+              },
+              onBuyTap: () {
+                // Implement buy functionality
+              },
+            ),
+          ),
         ],
       ),
     );
